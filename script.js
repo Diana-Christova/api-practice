d4109f7db2681b9a757bf6a75feb63b45688ad77
Diana Christova <dianalchristova@gmail.com>
2023-06-19 22:09:12 +0200

    this is my first commit in the api foder

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("Hello Word");
+const dinnerDiv = document.querySelector(".dinner");
+const dinnerButton = document
+
+//callback function 
+
+//promises 
+
+//async await
+
+const getMeal = async () => {
+   const data = await fetch(
+    "www.themealdb.com/api/json/v1/1/random.php"
+    );
+
+    const fullDataObject = await data.json();
+    const myMeal = fullDataObject.meals[0];
+
+    const mealDisplay = document.createElement ("div");
+
+//html
+//<div mealDisplay>
+
+    mealDisplay.innerHTML = '
+    <h4> ${myMeal.strMeal}</h4>
+    ';
+
+    dinnerDiv.append(mealDisplay);
+   } catch(error) {
+    console.log(error);
+    const errorMessage = document.createElement('div');
+    errorMessage.innerText = "Sorry could not load api data";
+    dinnerDiv.appendn (errorMessage);
+   }
+   };
+
+dinnerButton.addEventListener ("click", getMeal);
